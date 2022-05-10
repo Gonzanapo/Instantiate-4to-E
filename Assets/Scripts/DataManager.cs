641579ad29278925599ee29f7769e4f6d4d16dbc
Gonzanapo <88498679+Gonzanapo@users.noreply.github.com>
2022-05-10 15:31:47 -0300

    new script DataManager

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DataManager : MonoBehaviour
+{
+    public Text txtUserName;
+
+    public void ShowName()
+    {
+        Debug.Log("Hola " + txtUserName.text);
+    }
+
+}
